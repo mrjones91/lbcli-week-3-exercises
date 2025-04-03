00dbdf5ef394d933a10ca930cd0241c5e0fca73c
DJ <dj@code-crew.org>
2025-04-02 23:43:42 -0500

    4

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
 addy=$(bitcoin-cli -regtest getnewaddress -addresstype bech32)
-bitcoin-cli -regtest getaddressinfo $addy | jq '.pubkey'
+PUB_KEY=$(bitcoin-cli -regtest getaddressinfo $addy | jq -r '.pubkey')
